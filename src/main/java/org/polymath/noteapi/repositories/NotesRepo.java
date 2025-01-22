91147da58eb0e4c2a1ca9909b5fcb581bbbf8941
Yousouph Olosan <depolymathyousouph@gmail.com>
2025-01-22 10:54:11 +0100

    final commit to deploy

--- src/main/java/org/polymath/noteapi/repositories/NotesRepo.java
+++ src/main/java/org/polymath/noteapi/repositories/NotesRepo.java
@@ -4,6 +4,7 @@ import jakarta.persistence.NamedQuery;
 import org.polymath.noteapi.models.Notes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
